14def164bf7f4ec983cf9158d0662ca8770ec849
Stibbons <gaetan@xeberon.net>
2010-09-30 10:27:11 +0100

    Adding some eclipse projects

---
+++ src/cli/xcp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : xcp.cpp
+// Author      : Gaetan Semet
+// Version     :
+// Copyright   : (C) G. Semet 2010
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("xcp");
+	return EXIT_SUCCESS;
+}
